cc6ebbdd2ceb0fc96c50f3b8acc3568e5b07ace8
Chris <zawsx@users.noreply.github.com>
2017-01-12 22:29:12 +0000

    jhvbov

---
+++ rdetails/bridge/currank/js/szcr78VarInit.js
@@ -0,0 +1,3 @@
+var szul, szData2;
+// szData2=document.getElementById('szData2'); szdbprep('reviews');
+var szGenXMLpb=132,szGenXMLtb=192,szGenXMLfs='s', szGenXMLEvt='Wimbledon Village Cafe Bridge 2016';
