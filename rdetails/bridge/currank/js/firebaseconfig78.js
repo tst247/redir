b09c9e3a9fbab8f08f6c5a8535d08b4eba1e8f5f
Chris <zawsx@users.noreply.github.com>
2017-01-12 22:31:25 +0000

    sdfsadgv

---
+++ rdetails/bridge/currank/js/firebaseconfig78.js
@@ -0,0 +1,4 @@
+// authDomain: "your-app.firebaseapp.com", dbURL: "https://your-app.firebaseio.com", storageBucket: "your-app.appspot.com",
+var config = {apiKey: "AIzaSyD-c9UpjMDLRjnTPR3OVDJIcgYDdqpIqH8", authDomain: "bridge-current-rankings.firebaseapp.com",
+    databaseURL: "https://bridge-current-rankings.firebaseio.com", storageBucket: "bridge-current-rankings.appspot.com",
+    messagingSenderId: "1001187485133" }; firebase.initializeApp(config);
